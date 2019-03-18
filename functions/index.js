f1c155ce2ff9aacae0f49d0d1a74ae838e183fcd
Tamara Jantova <tami.jantova@gmail.com>
2019-03-18 11:09:07 +0100

    back end

--- functions/index.js
+++ functions/index.js
@@ -4,61 +4,146 @@ const admin = require('firebase-admin');
 
 admin.initializeApp();
 
-const database = admin.database().ref('/items');
+const database = admin.database().ref('/articles');
 
 
-const getItemsFromDatabase = (res) => {
-    let items = [];
+const getArticlesFromDatabase = (res) => {
+    let articles = [];
 
     return database.on('value', (snapshot) => {
-        snapshot.forEach((item) => {
-            items.push({
-                id: item.key,
-                item: item.val().item
+        snapshot.forEach((article) => {
+            articles.push({
+                id: article.key,
+                author: article.val().author,
+                category: article.val().category,
+                title: article.val().title,
+                text: article.val().text
             });
         });
-        res.status(200).json(items);
+        res.status(200).json(articles);
     }, (error) => {
         res.status(error.code).json({
             message: `Something went wrong. ${error.message}`
         })
     })
 };
-exports.addItem = functions.https.onRequest((req, res) => {
+exports.addArticle = functions.https.onRequest((req, res) => {
     return cors(req, res, () => {
         if(req.method !== 'POST') {
             return res.status(401).json({
                 message: 'Not allowed'
             })
         };
-        const item = req.body.item;
-        database.push({ item });
-        getItemsFromDatabase(res)
+
+        var author = req.body.author;
+        var category = req.body.category;
+        var title = req.body.title;
+        var text = req.body.text;
+
+        database.push({ 
+            author: author,
+            category: category,
+            title: title,
+            text: text
+        });
+        getArticlesFromDatabase(res)
     });
 });
 
-exports.getItems = functions.https.onRequest((req, res) => {
+exports.getArticles = functions.https.onRequest((req, res) => {
     return cors(req, res, () => {
         if(req.method !== 'GET') {
             return res.status(401).json({
                 message: 'Not allowed'
             });
         };
-        getItemsFromDatabase(res)
+        getArticlesFromDatabase(res)
     });
 });
 
-
-
-exports.deleteItem = functions.https.onRequest((req, res) => {
+exports.deleteArticle = functions.https.onRequest((req, res) => {
     return cors(req, res, () => {
         if(req.method !== 'DELETE') {
             return res.status(401).json({
                 message: 'Not allowed'
             })
         }
-        const id = req.query.id
-        admin.database().ref(`/items/${id}`).remove()
-        getItemsFromDatabase(res)
+        const id = req.body.id
+        admin.database().ref(`/articles/${id}`).remove()
+        getArticlesFromDatabase(res)
     })
 })
+
+exports.getArticleId = functions.https.onRequest((req, res) => {
+    
+    if(req.method === 'GET') {
+        
+        var key = req.query.key
+        
+        if(key) {
+            
+            return database.child(key).on('value', (snapshot) => {
+                if(snapshot.val()) {
+                    res.status(200).send({
+                        [key]: snapshot.val()
+                    });
+                }
+            })
+        }
+    }
+});
+
+exports.getArticleCategory = functions.https.onRequest((req, res) => {
+    let articles = [];
+    var category = req.query.category;
+
+    return database.orderByChild('category').equalTo(category).on('value', (snapshot) => {
+        
+        snapshot.forEach(function(childSnapshot) {
+
+            var key = childSnapshot.key;
+            
+            articles.push({
+                [key]: childSnapshot
+            });
+        });
+        res.status(200).send(articles);
+    }, (error) => {
+        res.status(error.code).json({
+            message: `Something went wrong. ${error.message}`
+        })
+    })
+});
+
+exports.updateArticle = functions.https.onRequest((req, res) => {
+    
+    if(req.method === 'PUT') {
+        
+        var id = req.query.id;
+
+        if(id) {
+
+            var text = req.body.text;
+            
+            if(text) {
+            
+                return admin.database().ref(`/articles/${id}`).on('value', (snapshot) => {
+                    if(snapshot.val()) {
+                        snapshot.ref.update({
+                            "text": text
+                        });
+
+                        return admin.database().ref(`/articles/${id}`).on('value', (snapshot) => {
+                            if(snapshot.val()) {
+                                var key = snapshot.key;
+                                res.status(200).send({
+                                    [key]: snapshot.val()
+                                });
+                            }
+                        })
+                    }
+                })
+            }
+        }
+    }
+});
